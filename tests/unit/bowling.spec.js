93d5846ca7a8bb20fa9ed9f7a9e662914a723709
Alexandr <alexandr@12knots.ru>
2021-01-26 19:33:09 +0100

    unit tests

--- tests/unit/bowling.spec.js
+++ tests/unit/bowling.spec.js
@@ -1,6 +1,7 @@
 import { mount, shallow, shallowMount, createLocalVue } from '@vue/test-utils'
-import store from '@/store'  
-// import knockedPins from '@/components/knockedPins.vue'
+// import store from '@/store'  
+import knockedPins from '@/components/knockedPins.vue'
+import Vuex from 'vuex'
 
 describe('store', () => {
   it('throwsLeft = 2', () => {
@@ -8,4 +9,30 @@ describe('store', () => {
   })
 })
 
+const localVue = createLocalVue();
+localVue.use(Vuex);
 
+
+const mockMutations = {
+  calculate: mutations.calculate()
+}
+const store = new Vuex.Store({
+  state: {},
+  mutations: mockMutations
+})
+
+localVue.use(Vuex)
+
+const wrapper = shallow(knockedPins, {
+	localVue,
+	store
+})
+
+describe('knockedPins test', () => {
+  
+  it('check test', () => {
+    
+    const test = wrapper.vm.items
+    console.log(test);
+  })
+})
