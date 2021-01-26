a6bb6c17a4ef2d60db525b1a637584d3b3c0f38c
Alexandr <alexandr@12knots.ru>
2021-01-26 19:33:44 +0100

    jest working

--- tests/unit/bowling.spec.js
+++ tests/unit/bowling.spec.js
@@ -1,38 +1,106 @@
-import { mount, shallow, shallowMount, createLocalVue } from '@vue/test-utils'
-// import store from '@/store'  
+import { mount, shallowMount, createLocalVue } from '@vue/test-utils'
+import realStore from '@/store'
 import knockedPins from '@/components/knockedPins.vue'
 import Vuex from 'vuex'
-
-describe('store', () => {
-  it('throwsLeft = 2', () => {
-    expect(store.state.throwsLeft).toBe(2);
-  })
-})
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
 
-
-const mockMutations = {
-  calculate: mutations.calculate()
-}
 const store = new Vuex.Store({
-  state: {},
-  mutations: mockMutations
+  state: {
+    frames: [
+      {
+        throwOne: "",
+        throwTwo: "",
+      },
+      {
+        throwOne: "",
+        throwTwo: "",
+      },
+      {
+        throwOne: "",
+        throwTwo: "",
+      },
+      {
+        throwOne: "",
+        throwTwo: "",
+      },
+      {
+        throwOne: "",
+        throwTwo: "",
+      },
+      {
+        throwOne: "",
+        throwTwo: "",
+      },
+      {
+        throwOne: "",
+        throwTwo: "",
+      },
+      {
+        throwOne: "",
+        throwTwo: "",
+      },
+      {
+        throwOne: "",
+        throwTwo: "",
+      },
+      {
+        throwOne: "",
+        throwTwo: "",
+        throwThree: "",
+      },
+    ],
+    scores: [],
+    mainScore: 0,
+    showedPins: [
+      true,
+      true,
+      true,
+      true,
+      true,
+      true,
+      true,
+      true,
+      true,
+      true,
+      true,
+    ],
+    throwNumber: 0,
+    turkey: false,
+    double: false,
+    strike: false,
+    spare: false,
+    bonus: 0,
+  },
+  mutations: {
+    renderInFrame(state, pins) { },
+    checkStrike(state, pins) { },
+    calculate(state, pins) { },
+    checkSpare(state, pins) { },
+    changeThrows(state, pins) { }
+  },
 })
 
 localVue.use(Vuex)
 
-const wrapper = shallow(knockedPins, {
-	localVue,
-	store
+const wrapper = shallowMount(knockedPins, {
+  localVue,
+  store
 })
 
-describe('knockedPins test', () => {
-  
-  it('check test', () => {
-    
-    const test = wrapper.vm.items
-    console.log(test);
+describe('knockedPins', () => {
+  it('Call calculate on click', () => {
+    const spy = spyOn(wrapper.vm, 'calculate')
+    const button = wrapper.findAll('.knocked-pins__button').at(-1);
+    button.trigger('click');
+    expect(wrapper.vm.calculate).toBeCalled()
+  })
+})
+describe('Calculate', () => {
+  it('2 + 2 = 4', () => {
+    realStore.commit('calculate', 2)
+    realStore.commit('calculate', 2)
+    expect(realStore.state.mainScore).toBe(4);
   })
 })
